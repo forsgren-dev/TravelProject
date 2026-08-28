0d862aa29d553a4ef500dd81421ef7cce33b0ff0
Erik Forsgren <forsgren.dev@gmail.com>
2026-08-28 13:50:26 +0200

    Add Attraction model

--- DbContext/MainDbContext.cs
+++ DbContext/MainDbContext.cs
@@ -11,5 +11,71 @@ namespace DbContext;
 
 public class MainDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
+    #region C# model of database tables
+    public DbSet<Attraction> Attraction { get; set; }
+    #endregion
 
-}
+    #region constructors
+    public MainDbContext() { }
+    public MainDbContext(DbContextOptions options) : base(options)
+    { }
+    #endregion
+
+    //Here we can modify the migration building
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        #region override modelbuilder
+        #endregion
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    //used by the child DbContexts to retrieve the connection string
+    protected string GetConnectionString(string connectionStringName)
+    {
+        // Design time: manually create configuration to read appsettings.json
+        var configBuilder = new ConfigurationBuilder()
+            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        var config = configBuilder.Build();
+        var connectionString = config.GetConnectionString(connectionStringName);
+        System.Console.WriteLine($"Design time Connection String from appsettings.json: {connectionString}");
+
+        return connectionString;
+    }
+
+    #region DbContext for some popular databases
+    public class SqlServerDbContext : MainDbContext
+    {
+        public SqlServerDbContext() { }
+        public SqlServerDbContext(DbContextOptions options) 
+            : base(options) { }
+
+
+        //Used only for CodeFirst Database Migration and database update commands
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = GetConnectionString("SqlServerDocker");
+                optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure());
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<decimal>().HaveColumnType("money");
+            configurationBuilder.Properties<string>().HaveColumnType("varchar(200)");
+
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Add your own modelling based on done migrations
+            base.OnModelCreating(modelBuilder);
+        }
+    }
---
+++ Models/Attraction.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Attraction
+    {
+        
+        public virtual Guid AttractionId { get; set; }
+        public virtual string AttractionName { get; set; }
+        public virtual string AttractionDescription { get; set; }
+        public virtual string AttractionStreet { get; set; }
+        public virtual string AttractionZip { get; set; }
+        public virtual Guid CityId { get; set; }
+
+
+    }
+}
